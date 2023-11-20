fe5bb94bd4baf397feb893645007dbf9fa00ec6d
Archana-2004 <126542843+Archana-2004@users.noreply.github.com>
2023-11-20 23:33:15 +0530

    Add files via upload

---
+++ home/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+
+    class Meta:
+        app_label = 'home'
+    taskTitle=models.CharField(max_length=30)
+    desc=models.TextField()
+    time=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.taskTitle)
+    
